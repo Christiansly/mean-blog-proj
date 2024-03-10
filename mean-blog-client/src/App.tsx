e5f6f7b52baaad16a7f00e4bdd96bc7e783c768c
Christiansly <abiode23@gmail.com>
2024-03-10 20:04:02 +0100

    Refactor routing and signup page

--- mean-blog-client/src/App.tsx
+++ mean-blog-client/src/App.tsx
@@ -1,25 +1,26 @@
-import { BrowserRouter, Routes, Route} from "react-router-dom"
-import Home from "./pages/Home"
-import About from "./pages/About"
-import Signup from "./pages/Signup"
-import Signin from "./pages/Signin"
-import Projects from "./pages/Projects"
-import Dashboard from "./pages/Dashboard"
-import Header from "./components/Header"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Signup from "./pages/Signup";
+import Signin from "./pages/Signin";
+import Projects from "./pages/Projects";
+import Dashboard from "./pages/Dashboard";
+import Header from "./components/Header";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/sign-up" element={<Signup />} />
-        <Route path="/sign-in" element={<Signin />} />
-        <Route path="projects" element={<Projects/>} />
-        <Route path="/dashboard" element={<Dashboard/>} />
-
-      </Routes>
-    </BrowserRouter>
-  )
+    <div className="dark:text-gray-400 dark:bg-gray-900">
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/sign-up" element={<Signup />} />
+          <Route path="/sign-in" element={<Signin />} />
+          <Route path="projects" element={<Projects />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
--- mean-blog-client/src/pages/Signup.tsx
+++ mean-blog-client/src/pages/Signup.tsx
@@ -1,7 +1,64 @@
-import React from 'react'
+import { Button, Label, TextInput } from "flowbite-react";
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Signup() {
   return (
-    <div>Signup</div>
-  )
+    <div className="min-h-screen mt-20 dark:bg-primary">
+      <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row gap-10">
+        <div className="flex-1">
+          <Link
+            to="/"
+            className="font-bold dark:text-white text-4xl dark:bg-black py-2 px-3 rounded-md"
+          >
+            <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
+              Christian's
+            </span>{" "}
+            Blog
+          </Link>
+          <p className="text-sm mt-5 dark:text-slate-300">
+            A blog about web development, programming, and other tech-related
+            topics. You can sign up with your email
+          </p>
+        </div>
+        <div className="flex-1">
+          <form>
+            <div className="">
+              <Label value="Your username" />
+              <TextInput
+                type="text"
+                placeholder="Your username"
+                id="username"
+              />
+            </div>
+            <div className="">
+              <Label value="Your email" />
+              <TextInput type="text" placeholder="Your email" id="email" />
+            </div>
+            <div className="">
+              <Label value="Your password" />
+              <TextInput
+                type="text"
+                placeholder="Your password"
+                id="password"
+              />
+            </div>
+            <Button
+              type="submit"
+              gradientDuoTone="purpleToPink"
+              className="mt-5 w-full"
+            >
+              Sign up
+            </Button>
+          </form>
+          <div className="flex gap-2 text-sm mt-5">
+            <span>Have an account already?</span>
+            <Link to="/sign-in" className="text-purple-500">
+              Sign in
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
